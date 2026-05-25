18f27d4012e2c5246c842a53d8b5859cbfbc8a32
Rohit1024 <rohitkharche1024@gmail.com>
2026-05-25 10:48:10 +0530

    feat: replace manual mermaid script injection with astro-mermaid integration

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
+import mermaid from 'astro-mermaid'
 
 import expressiveCode from 'astro-expressive-code'
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
@@ -20,6 +21,10 @@ import { remarkPlugins } from './plugins'
 export default defineConfig({
   site: 'https://rohitkharche.web.app',
   integrations: [
+    mermaid({
+      autoTheme: true,
+      enableLog: false,
+    }),
     expressiveCode({
       themes: ['github-light', 'github-dark'],
       plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
